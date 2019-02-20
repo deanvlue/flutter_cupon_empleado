287735a09e594733454f9669c4ed83cfbc355784
José Carlos Muñoz Méndez <jmunozm@alsea.com.mx>
2019-02-19 18:24:49 -0600

    Shows cupon on top

--- lib/main.dart
+++ lib/main.dart
@@ -5,58 +5,54 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new MaterialApp(
-      title: 'Primer FLutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Cupón de empleado Alsea'),
+      title: 'Flutter layout demo',
+      home:Scaffold(
+        backgroundColor: new Color(0xff00597f),
+        appBar: AppBar(
+          title: new Text('Flutter Layout demo'),
         ),
-        //backgroundColor: Color.fromARGB(0, 110, 196, 231),
-        //backgroundColor: Colors.blueAccent,
-        backgroundColor: new Color(0xFF1385b3),
-        body: new Container(
-          padding: const EdgeInsets.all(10.0),
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
+        body:ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    new Text("Carlos Muñoz"),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            child:    
+                              Text('Carlos Muñoz',
+                                style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 34,
+                                fontWeight: FontWeight.w200,
+                                ),
+                              ),
+                          )
+                        ]
+                      ),
+                    )
                   ],
                 ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    new Text('3MPD0FKFR3'),
+                    Image.asset(
+                      'images/cupon_empleado.png', 
+                      fit:BoxFit.cover
+                    )
                   ],
-                )
-                ],
-
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    flex :2,
-                    child: new Image.asset('images/cupon_empleado.png', fit:BoxFit.cover),
-                  )
-                ],
-              ),
-            ],
-            /*children: <Widget>[
-              new Text("José Carlos Muñoz"),
-              //new Image.network('https://webapp-mx-corp-prod-wscommons.azurewebsites.net/WSBarcode/barcode-api/image?barcode=3MPD0FKFR3&type=1'),
-              new Image.asset('images/cupon_empleado.png', fit:BoxFit.cover),
-              new Text('3MPD0FKFR3')
-              //new Image.network('https://cdn2.thecatapi.com/images/4if.gif')
-            ]*/
-          )
-        ),
-      ),
+                ),
+              ],
+            )
+          ],
+        )
+      )
     );
   }
 }
